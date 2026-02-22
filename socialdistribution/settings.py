414f8cf7d59c7cf97205e183d4ac22f2cc1b807e
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-02-22 12:11:25 -0700

    Set up for heroku

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b(*&m5$4*8f0f*&w8atqbv813i=cko^$84k$m89zv(_#gb73*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,4 +130,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATIC_URL = "/static/"
