f70d3602a1dc5ce2a17568b92898b86b0ee5d0a7
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-02-25 21:33:26 -0700

    Create serial and fqid fields for models
    Follow intentionally doesn't have it, i don't think it needs it

--- munch/models.py
+++ munch/models.py
@@ -5,9 +5,17 @@ import uuid
 # possible tables needed for project
 
 class Author(models.Model):
-    serial = models.UUIDField(default=uuid.uuid4)
     username = models.CharField(max_length=200)
     displayName = models.CharField(max_length=200)
+
+    serial = models.UUIDField(default=uuid.uuid4)
+    fqid = models.URLField(blank=True, unique=True)
+
+    # override save to generate fqid from serial if no fqid is provided
+    def save(self, *args, **kwargs):
+        if not self.fqid:
+            self.fqid = f"https://host/munch/api/authors/{self.serial}"
+        return super().save(*args, **kwargs)
 
 class Entry(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -18,6 +26,14 @@ class Entry(models.Model):
     contentType = models.CharField(max_length=200)
     content = models.TextField()
 
+    serial = models.UUIDField(default=uuid.uuid4)
+    fqid = models.URLField(blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.fqid:
+            self.fqid = f"https://host/munch/api/authors/{self.author.serial}/entries/{self.serial}"
+        return super().save(*args, **kwargs)
+
 class Comment(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
@@ -25,10 +41,26 @@ class Comment(models.Model):
     contentType = models.CharField(max_length=200)
     comment = models.TextField()
 
+    serial = models.UUIDField(default=uuid.uuid4)
+    fqid = models.URLField(blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.fqid:
+            self.fqid = f"https://host/munch/api/authors/{self.author.serial}/commented/{self.serial}"
+        return super().save(*args, **kwargs)
+
 class Like(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     object = models.ForeignKey(Entry, on_delete=models.CASCADE)
     published = models.DateTimeField(default=datetime.now)
+
+    serial = models.UUIDField(default=uuid.uuid4)
+    fqid = models.URLField(blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.fqid:
+            self.fqid = f"https://host/munch/api/authors/{self.author.serial}/liked/{self.serial}"
+        return super().save(*args, **kwargs)
 
 class Follow(models.Model):
     actor = models.ForeignKey(Author, on_delete=models.CASCADE)
