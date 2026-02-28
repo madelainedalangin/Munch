ad4517e20b8f1cd9577e127c12398645b1ed00f8
x4nni <xflores151@gmail.com>
2026-02-28 16:26:06 -0700

    Adjusted login and followers.html to be compatible with recent identity merge

--- munch/urls.py
+++ munch/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='munch/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login-success/', views.login_success_redirect, name='login_success'),
+
+    path('followers/', views.FollowersView.as_view(), name="followers"),
     
     # path('', views.IndexView.as_view(), name='index'),
 
