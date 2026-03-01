24a31c866e397b8d18929ed53b96144bda897e58
Madelaine Dalangin <madelainedalangin@gmail.com>
2026-03-01 03:45:09 -0700

    fixed logour redirect url and added login url

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = "/static/"
 AUTH_USER_MODEL = 'munch.Author'
 
 LOGIN_REDIRECT_URL = '/munch/login-success/'  # Redirect to this URL after successful login
-LOGOUT_REDIRECT_URL = '/munch/login'
+LOGOUT_REDIRECT_URL = '/munch/login/'
+LOGIN_URL = '/munch/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
