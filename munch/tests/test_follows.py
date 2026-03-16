4d09cadf832a4bbf1c21f8bc38a4e9866a1f9770
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-03-16 10:43:32 -0600

    updates test_follows.py

--- munch/tests/test_follows.py
+++ munch/tests/test_follows.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.conf import settings
 from django.utils import timezone
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from munch.models import Author, Entry, Comment, Like, Follow
 from datetime import timedelta
@@ -16,14 +16,102 @@ import unittest
 # FOLLOWING/FRIENDS USER STORY TEST #
 ####################################
 
-class FollowAPITest(TestCase):
+class GetFollowingTestCase(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
 
         self.alice = Author.objects.create_user(
-            username='test_author',
+            username='alice',
             password='123',
-            displayName='test author',
+            displayName='Alice',
             is_approved=True
         )
+
+        self.bob = Author.objects.create_user(
+            username='bob',
+            password='123',
+            displayName='Bob',
+            is_approved=True
+        )
+
+        self.eddie = Author.objects.create_user(
+            username='eddie',
+            password='123',
+            displayName='Eddie',
+            is_approved=True
+        )
+
+
+        
+        self.url = reverse('munch:get_following', kwargs={'author_serial': self.alice.uuid})
+
+    def test_get_following(self):
+        pass
+
+    def test_unauthenticated(self):
+        pass
+
+
+class ManageFollowingTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.alice = Author.objects.create_user(
+            username='alice',
+            password='123',
+            displayName='Alice',
+            is_approved=True
+        )
+
+        self.bob = Author.objects.create_user(
+            username='bob',
+            password='123',
+            displayName='Bob',
+            is_approved=True
+        )
+
+        self.url = reverse('munch:manage_following', kwargs={'author_serial': self.alice.uuid, 'target_fqid': self.bob.id})
+
+class ManageFollowerTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.alice = Author.objects.create_user(
+            username='alice',
+            password='123',
+            displayName='Alice',
+            is_approved=True
+        )
+
+        self.bob = Author.objects.create_user(
+            username='bob',
+            password='123',
+            displayName='Bob',
+            is_approved=True
+        )
+
+        self.url = reverse('munch:manage_follower', kwargs={'author_serial': self.alice.uuid, 'target_fqid': self.bob.id})
+
+class GetFollowRequestsTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.alice = Author.objects.create_user(
+            username='alice',
+            password='123',
+            displayName='Alice',
+            is_approved=True
+        )
+
+        self.bob = Author.objects.create_user(
+            username='bob',
+            password='123',
+            displayName='Bob',
+            is_approved=True
+        )
+
+        self.url = reverse('munch:get_follow_requests', kwargs={'author_serial': self.alice.uuid})
