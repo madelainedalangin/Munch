1d42eda87b9ef7d742f0d03f011d48d3990f230e
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-03-16 13:25:48 -0600

    add test for get following

--- munch/tests/test_follows.py
+++ munch/tests/test_follows.py
@@ -1,14 +1,8 @@
-from django.test import TestCase, Client
 from django.urls import reverse
-from django.conf import settings
-from django.utils import timezone
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
-from munch.models import Author, Entry, Comment, Like, Follow
-from datetime import timedelta
-import uuid
-from unittest.mock import patch
-from munch.utils import sync_github_activity
+from munch.models import Author, Follow
+from munch.serializers import AuthorSerializer, FollowRequestSerializer
 
 import unittest
 
@@ -18,100 +12,161 @@ import unittest
 
 class GetFollowingTestCase(APITestCase):
 
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
 
-        self.alice = Author.objects.create_user(
+        cls.alice = Author.objects.create_user(
             username='alice',
             password='123',
             displayName='Alice',
             is_approved=True
         )
 
-        self.bob = Author.objects.create_user(
+        cls.bob = Author.objects.create_user(
             username='bob',
             password='123',
             displayName='Bob',
             is_approved=True
         )
 
-        self.eddie = Author.objects.create_user(
+        cls.eddie = Author.objects.create_user(
             username='eddie',
             password='123',
             displayName='Eddie',
             is_approved=True
         )
 
-
-        
-        self.url = reverse('munch:get_following', kwargs={'author_serial': self.alice.uuid})
-
-    def test_get_following(self):
-        pass
+        cls.url = reverse('munch:get_following', kwargs={'author_serial': cls.alice.uuid})
 
     def test_unauthenticated(self):
-        pass
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        
+    def test_no_following(self):
+        self.client.login(username='alice', password='123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_following_list(self):
+
+        follow_alice_bob = Follow.objects.create(
+            actor=self.alice,
+            object=self.bob,
+            status='accepted'
+        )
+
+        follow_alice_eddie = Follow.objects.create(
+            actor=self.alice,
+            object=self.eddie,
+            status='accepted'
+        )
+
+        self.client.login(username='alice', password='123')
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertIn(AuthorSerializer(self.bob).data, response.data)
+        self.assertIn(AuthorSerializer(self.eddie).data, response.data)
 
 class ManageFollowingTestCase(APITestCase):
 
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
 
-        self.alice = Author.objects.create_user(
+        cls.alice = Author.objects.create_user(
             username='alice',
             password='123',
             displayName='Alice',
             is_approved=True
         )
 
-        self.bob = Author.objects.create_user(
+        cls.bob = Author.objects.create_user(
             username='bob',
             password='123',
             displayName='Bob',
             is_approved=True
         )
 
-        self.url = reverse('munch:manage_following', kwargs={'author_serial': self.alice.uuid, 'target_fqid': self.bob.id})
+        cls.eddie = Author.objects.create_user(
+            username='eddie',
+            password='123',
+            displayName='Eddie',
+            is_approved=True
+        )
+
+        cls.url = reverse('munch:get_following', kwargs={'author_serial': cls.alice.uuid})
+
+    def test_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    
 
 class ManageFollowerTestCase(APITestCase):
 
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
 
-        self.alice = Author.objects.create_user(
+        cls.alice = Author.objects.create_user(
             username='alice',
             password='123',
             displayName='Alice',
             is_approved=True
         )
 
-        self.bob = Author.objects.create_user(
+        cls.bob = Author.objects.create_user(
             username='bob',
             password='123',
             displayName='Bob',
             is_approved=True
         )
 
-        self.url = reverse('munch:manage_follower', kwargs={'author_serial': self.alice.uuid, 'target_fqid': self.bob.id})
+        cls.eddie = Author.objects.create_user(
+            username='eddie',
+            password='123',
+            displayName='Eddie',
+            is_approved=True
+        )
+
+        cls.url = reverse('munch:get_following', kwargs={'author_serial': cls.alice.uuid})
+
+    def test_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
 
 class GetFollowRequestsTestCase(APITestCase):
 
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
 
-        self.alice = Author.objects.create_user(
+        cls.alice = Author.objects.create_user(
             username='alice',
             password='123',
             displayName='Alice',
             is_approved=True
         )
 
-        self.bob = Author.objects.create_user(
+        cls.bob = Author.objects.create_user(
             username='bob',
             password='123',
             displayName='Bob',
             is_approved=True
         )
 
-        self.url = reverse('munch:get_follow_requests', kwargs={'author_serial': self.alice.uuid})
+        cls.eddie = Author.objects.create_user(
+            username='eddie',
+            password='123',
+            displayName='Eddie',
+            is_approved=True
+        )
+
+        cls.url = reverse('munch:get_following', kwargs={'author_serial': cls.alice.uuid})
+
+    def test_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
