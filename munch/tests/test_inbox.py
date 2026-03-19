2c9e7ca27d2bd71ca7980e120fa871f05bc8d8be
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-03-19 12:50:11 -0600

    fix inbox tests

--- munch/tests/test_inbox.py
+++ munch/tests/test_inbox.py
@@ -23,45 +23,47 @@ class InboxAPITest(APITestCase):
 
         cls.bob_remote_data = {
             'type': 'author',
-            'id': 'http://remotenode/api/authors/2051213d-d4b4-4cf9-ba57-f2922b39dfa9',
-            'web': 'http://remotenode/authors/2051213d-d4b4-4cf9-ba57-f2922b39dfa9',
+            'id': 'http://remotenode.herokuapp.com/api/authors/2051213d-d4b4-4cf9-ba57-f2922b39dfa9',
+            'host': 'http://remotenode.herokuapp.com/api/',
             'displayName':'Bob',
             'github': None,
-            'profileImage': None
+            'profileImage': None,
+            'web': 'http://remotenode.herokuapp.com/authors/2051213d-d4b4-4cf9-ba57-f2922b39dfa9'
         }
 
-        cls.url = reverse('munch:inbox', kwargs={'target_serial': cls.alice.uuid})
+        cls.url = reverse('munch:inbox', kwargs={'target_serial': cls.alice_local.uuid})
 
-    @skip('fix authors model first')
     def test_post_follow(self):
 
         remote_api_follow = {
             'type': 'follow',
             'summary': 'Bob wants to follow Alice',
             'actor': self.bob_remote_data,
-            'object': AuthorSerializer(self.alice_local).validated_data
+            'object': AuthorSerializer(self.alice_local).data
         }
 
-        response = self.client.post(self.url, remote_api_follow, format='json')
+        response = self.client.post(self.url, data=remote_api_follow, format='json')
 
         # check status code
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # check if a stub for bob was created
-        bob_stub = Author.objects.filter(id='http://remotenode/api/authors/2051213d-d4b4-4cf9-ba57-f2922b39dfa9')
-        self.assertTrue(bob_stub.exists())
+        bob_stub_query = Author.objects.filter(id=self.bob_remote_data.get('id'))
+        self.assertTrue(bob_stub_query.exists())
+        bob_stub = bob_stub_query.first()
+        self.assertIsNone(bob_stub.username)
 
         # check if follow request was created
-        self.assertTrue(Follow.objects.filter(actor=bob_stub, object=self.alice_local))
+        self.assertTrue(Follow.objects.filter(actor=bob_stub, object=self.alice_local).exists())
 
-    @skip('fix authors model first')
+    @skip('not implemented yet')
     def test_post_entry(self):
         pass
 
-    @skip('fix authors model first')
+    @skip('note implemented yet')
     def test_post_like(self):
         pass
 
-    @skip('fix authors model first')
+    @skip('not implemented yet')
     def test_post_comment(self):
         pass
