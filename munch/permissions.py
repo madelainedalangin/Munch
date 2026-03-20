8d356dc25876a86478616aafc55ea645a35d8ede
delacrxz <155778461+delacrxz@users.noreply.github.com>
2026-03-19 18:54:57 -0600

    update IsAuthorizedServer permission

--- munch/permissions.py
+++ munch/permissions.py
@@ -2,10 +2,11 @@ from rest_framework.permissions import BasePermission
 from munch.models import Server
 
 class IsAuthorizedServer(BasePermission):
-    message = 'Access restricted to authorized nodes'
+    message = 'Access restricted to authorized remote nodes'
 
     def has_permission(self, request, view):
-        return ( request.auth == 'basic'
+        return ( 
+            request.auth == 'basic'
             and isinstance(request.user, Server)
             and request.user.is_approved
         )
